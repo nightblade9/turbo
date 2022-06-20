98ef779600a1b822e91756903e48e024664fe529
nightblade9 <nightbladecodes@gmail.com>
2022-06-19 21:41:06 -0400

    Splash scene!

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Batteries-included template for 2D pixel-art web and desktop Godot games.
 
 Current project template is for Godot v3.4.4.
 
+# Usage
+
+TBD
+
 # Limitations
 
 ## Display ##
---
+++ source/contexts/pregame/scenes/SplashScene.gd
@@ -0,0 +1,30 @@
+extends Control
+
+signal done()
+
+const _FADE_SECONDS:float = 1.0
+const _WAIT_SECONDS:float = 3.3
+
+onready var _logo = $TextureRect
+
+func _ready():
+	if OS.has_feature("debug"):
+		call_deferred("_on_fade_out_done")
+		#return
+		
+	var tween = Tween.new()
+	add_child(tween)
+	tween.interpolate_property(_logo, "self_modulate", Color(1, 1, 1, 0), Color(1, 1, 1, 1), _FADE_SECONDS)
+	tween.connect("tween_all_completed", self, "_on_fade_in_done")
+	tween.start()
+	
+func _on_fade_in_done():
+	yield(get_tree().create_timer(_WAIT_SECONDS), "timeout")
+	var tween = Tween.new()
+	add_child(tween)
+	tween.interpolate_property(_logo, "self_modulate", Color(1, 1, 1, 1), Color(1, 1, 1, 0), _FADE_SECONDS)
+	tween.connect("tween_all_completed", self, "_on_fade_out_done")
+	tween.start()
+
+func _on_fade_out_done():
+	emit_signal("done")
