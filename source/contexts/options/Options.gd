3cf8c2ffa072d07207549cc2c61faaa3e2154248
nightblade9 <nightbladecodes@gmail.com>
2022-06-22 19:52:37 -0400

    Refactor: turn Options into a Resource Makes loading/saving much easier

--- source/contexts/options/Options.gd
+++ source/contexts/options/Options.gd
@@ -1,55 +1,21 @@
-extends Node
+extends Resource
 
-const _OPTIONS_FILE_NAME = "user://options.dat"
+const _OPTIONS_FILE_NAME = "user://options.tres"
 
-const AMBIENCE_BUS_NAME = "BGSE"
-const SFX_BUS_NAME = "SFX"
+export var bgm_volume:float = 1.0
+export var ambience_volume:float = 1.0
+export var sfx_volume:float = 1.0
+export var invincible:bool = false
+export var screen_shake:bool = true
 
-# DEFAULTS go in here.
-var DEFAULTS = {
-	"bgm_volume": 1,
-	"ambience_volume": 1,
-	"sfx_volume": 1,
-	"invincible": false,
-	"screen_shake": true
-}
-
-var data = {}
-
-func save_to_disk() -> void:
-	var json_blob = to_json(self.data)
-
-	var options_file = File.new()
-	options_file.open(_OPTIONS_FILE_NAME, File.WRITE)
-	options_file.store_string(json_blob)
-	options_file.close()
-
-func _init() -> void:
-	data = DEFAULTS # Prevent crash on first run
-	_load_from_disk()
-
-	# Update volume levels
-	update_audio_volume(AMBIENCE_BUS_NAME, data["ambience_volume"])
-	update_audio_volume(SFX_BUS_NAME, data["sfx_volume"])
-
-# Value is a float from 0..1
-func update_audio_volume(bus_name:String, value:float):
-	var decibels = linear2db(value)
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index(bus_name), decibels)
-
-func _load_from_disk() -> void:
-	var options_file = File.new()
-	if options_file.file_exists(_OPTIONS_FILE_NAME):
-		options_file.open(_OPTIONS_FILE_NAME, File.READ)	
-		var json_blob = options_file.get_as_text()
-		options_file.close()
-		
-		var result = JSON.parse(json_blob)
-		if result.error:
-			push_error("Error reading options data: %s (line %s)" % [result.error_string, result.error_line])
-		else:
-			var loaded_data = result.result
-			# Apply loaded_data on top of Options.data, so that any keys that are missing
-			# from our options file, will get the defaults instead.
-			for key in loaded_data.keys():
-				self.data[key] = loaded_data[key]
+func save_data():
+	var result = ResourceSaver.save(_OPTIONS_FILE_NAME, self)
+	if result != OK:
+		push_error("Failed to save data! Result was: %s" % result)
+	
+static func load_data() -> Resource:
+	if ResourceLoader.exists(_OPTIONS_FILE_NAME):
+		return load(_OPTIONS_FILE_NAME)
+	
+	# Returns a new instance
+	return load("res://contexts/options/Options.gd").new()
--- source/contexts/options/OptionsDialog.gd
+++ source/contexts/options/OptionsDialog.gd
@@ -2,58 +2,49 @@ extends CanvasLayer
 
 const Options = preload("res://contexts/options/Options.gd")
 
+const AMBIENCE_BUS_NAME = "BGSE"
+const SFX_BUS_NAME = "SFX"
+
 onready var _window_dialog = $WindowDialog
 
-onready var _music_volume = $WindowDialog/MarginContainer/VBoxContainer/MusicVolume
 onready var _bgse_volume = $WindowDialog/MarginContainer/VBoxContainer/BgseVolume
 onready var _sfx_volume = $WindowDialog/MarginContainer/VBoxContainer/SfxVolume
 
-onready var _music_slider = $WindowDialog/MarginContainer/VBoxContainer/MusicVolumeSlider
 onready var _ambience_slider = $WindowDialog/MarginContainer/VBoxContainer/AmbienceVolumeSlider
 onready var _sfx_slider = $WindowDialog/MarginContainer/VBoxContainer/SoundEffectsVolumeSlider
 
 onready var _invincible_toggle = $WindowDialog/MarginContainer/VBoxContainer/InvincibilityCheck
 onready var _screen_shake_toggle = $WindowDialog/MarginContainer/VBoxContainer/ScreenShakeCheck
 
-onready var _in_game_options_separator = $WindowDialog/MarginContainer/VBoxContainer/HSeparator2
-onready var _restart_button = $WindowDialog/MarginContainer/VBoxContainer/RestartButton
-onready var _shop_button = $WindowDialog/MarginContainer/VBoxContainer/SkillShopButton
-
 signal close()
 
-var _options:Options = Options.new()
+var _options:Options = Options.load_data()
 
 func _ready():
 	# Update UIs to match persisted options
-	_update_volume_uis()
+	_update_bus_volumes_and_uis()
 	
-	_ambience_slider.value = _options.data["ambience_volume"]
-	_sfx_slider.value = _options.data["sfx_volume"]
+	_ambience_slider.value = _options.ambience_volume
+	_sfx_slider.value = _options.sfx_volume
 	
-	_invincible_toggle.pressed = _options.data["invincible"]
-	_screen_shake_toggle.pressed = _options.data["screen_shake"]
+	_invincible_toggle.pressed = _options.invincible
+	_screen_shake_toggle.pressed = _options.screen_shake
 
 func popup():
 	_window_dialog.popup_centered()
-	_invincible_toggle.pressed = _options.data["invincible"]
-	_screen_shake_toggle.pressed = _options.data["screen_shake"]
+	_invincible_toggle.pressed = _options.invincible
+	_screen_shake_toggle.pressed = _options.screen_shake
 	
 	# Pause game on open
 	get_tree().paused = true
 
-func hide_in_game_options():
-	# Hide things that shouldn't show up when accessed from title
-	_restart_button.queue_free()
-	_in_game_options_separator.queue_free()
-	_shop_button.queue_free()
-
 func _on_InvincibilityCheck_toggled(button_pressed:bool):
-	_options.data["invincible"] = button_pressed
-	_options.save_to_disk()
+	_options.invincible = button_pressed
+	_options.save_data()
 
 func _on_ScreenShakeCheck_toggled(button_pressed:bool):
-	_options.data["screen_shake"] = button_pressed
-	_options.save_to_disk()
+	_options.screen_shake = button_pressed
+	_options.save_data()
 	
 func _on_WindowDialog_popup_hide():
 	emit_signal("close")
@@ -68,19 +59,28 @@ func _input(event:InputEvent):
 			_window_dialog.hide()
 
 func _on_AmbienceVolumeSlider_value_changed(value):
-	_options.data["ambience_volume"] = value
+	_options.ambience_volume = value
 	_update_volume_uis()
-	_options.update_audio_volume(_options.AMBIENCE_BUS_NAME, value)
-	_options.save_to_disk()
+	update_audio_volume(AMBIENCE_BUS_NAME, value)
+	_options.save_data()
 
 func _on_SoundEffectsVolumeSlider_value_changed(value):
-	_options.data["sfx_volume"] = value
+	_options.sfx_volume = value
 	_update_volume_uis()
-	_options.update_audio_volume(_options.SFX_BUS_NAME, value)
-	_options.save_to_disk()
+	update_audio_volume(SFX_BUS_NAME, value)
+	_options.save_data()
 
-# Meh. One method for all three. IDK, splitting seems wasteful?
+func _update_bus_volumes_and_uis():
+	# Update the audio buses
+	update_audio_volume(AMBIENCE_BUS_NAME, _options.ambience_volume)
+	update_audio_volume(SFX_BUS_NAME, _options.sfx_volume)
+	_update_volume_uis()
+
 func _update_volume_uis():
-	_bgse_volume.text = "Background Ambience Volume (%s%%)" % [int(_options.data["ambience_volume"] * 100)]
-	_sfx_volume.text = "Sound Effects Volume (%s%%)" % [int(_options.data["sfx_volume"] * 100)]
+	# Show the correct values
+	_bgse_volume.text = "Background Ambience Volume (%s%%)" % [int(_options.ambience_volume * 100)]
+	_sfx_volume.text = "Sound Effects Volume (%s%%)" % [int(_options.sfx_volume * 100)]
 	
+func update_audio_volume(bus_name:String, value:float):
+	var decibels = linear2db(value)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index(bus_name), decibels)
