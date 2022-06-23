fe7ea51d0721487c6152639953675445c07f9b59
nightblade9 <nightbladecodes@gmail.com>
2022-06-22 21:49:38 -0400

    Save data; it's similar to options.

---
+++ source/contexts/save/SaveData.gd
@@ -0,0 +1,20 @@
+extends Resource
+
+const _SAVE_FILE_NAME = "user://save-%s.tres"
+
+# Put your save fields here, as export variables. They can be native
+# types, or resources (for recursive saving).
+
+func save_data(save_slot:String) -> void:
+	var save_file = _SAVE_FILE_NAME % save_slot
+	var result = ResourceSaver.save(save_file, self)
+	if result != OK:
+		push_error("Failed to save data to %s! Result was: %s" % [save_slot, result])
+	
+static func load_data(save_slot:String) -> Resource:
+	var save_file = _SAVE_FILE_NAME % save_slot
+	
+	if ResourceLoader.exists(save_file):
+		return load(save_file)
+	
+	return null
