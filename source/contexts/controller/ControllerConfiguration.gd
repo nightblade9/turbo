ae38d5422ee49ffa8390995ba31b9463afba7ccb
nightblade9 <nightbladecodes@gmail.com>
2022-06-23 22:00:08 -0400

    First cut of controller config: show buttons/arrows pressed.

---
+++ source/contexts/controller/ControllerConfiguration.gd
@@ -0,0 +1,10 @@
+extends Control
+
+onready var _label = $Label
+
+func _unhandled_input(event):
+	if event is InputEventJoypadButton and event.is_pressed():
+		_label.text = "Pressed: %s" % event.as_text()
+	elif event is InputEventJoypadMotion and event.is_pressed():
+		var motion = event as InputEventJoypadMotion
+		_label.text = "Move: %s" % motion.as_text()
