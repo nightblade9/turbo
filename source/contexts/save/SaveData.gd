622909e22664ad138328723075624730d221aa2a
nightblade9 <nightbladecodes@gmail.com>
2024-05-21 19:37:48 -0400

    Auto-save elapsed game time.

--- source/contexts/save/SaveData.gd
+++ source/contexts/save/SaveData.gd
@@ -7,7 +7,14 @@ const _SAVE_FILE_NAME = "user://save%s.tres"
 # types, or resources (for easy recursive saving).
 @export var game_time_seconds:int = 0
 
+var loaded_on:float
+
 func save_data(save_slot:String) -> void:
+	var elapsed_seconds = (Time.get_ticks_msec() - loaded_on) / 1000
+	game_time_seconds += elapsed_seconds
+	# Save again before quitting? Record just the elapsed diff.
+	loaded_on = Time.get_ticks_msec()
+	
 	var save_file = _SAVE_FILE_NAME % save_slot
 	var result = ResourceSaver.save(self, save_file)
 	if result != OK:
@@ -17,6 +24,8 @@ static func load_data(save_slot:String) -> Resource:
 	var save_file = _SAVE_FILE_NAME % save_slot
 	
 	if ResourceLoader.exists(save_file):
-		return load(save_file)
+		var data:SaveData = load(save_file)
+		data.loaded_on = Time.get_ticks_msec()
+		return data
 	
 	return null
